85a8d52f5f4da928f1f4948f64a879b28ee581c5
dzy407720698@gmail.com <zydeng@ucdavis.edu>
2019-09-19 19:29:11 -0400

    add

--- magic_eight.py
+++ magic_eight.py
@@ -3,3 +3,8 @@
 def getQuestion():
     question = input("What is your question?")
     return question
+
+magic_ball_answers = ["It is decidedly so","It is certain","Without a doubt",
+                      "Yes - definitely","You may rely on it",
+                      "As I see it, yes","Most likely","Outlook good",
+                      "Yes","Signs point to yes"]
