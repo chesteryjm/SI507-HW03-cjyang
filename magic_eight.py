a0d9b3c158f060cbf9e3e95338ec99fa65c02b77
cjyang <cjyang@umich.edu>
2019-09-19 20:25:45 -0400

    check question

--- magic_eight.py
+++ magic_eight.py
@@ -1,16 +1,29 @@
 ## This is the init code for HW3
+import random
 
 def getQuestion():
     question = input("What is your question?")
     return question
 
+magic_ball_answers = ["It is decidedly so","It is certain","Without a doubt",
+                      "Yes - definitely","You may rely on it",
+                      "As I see it, yes","Most likely","Outlook good",
+                      "Yes","Signs point to yes",'Reply hazy, try again.',
+                      'Ask again later.', 'Better not tell you now.',
+                      'Cannot predict now.','Concentrate and ask again.',
+                      'Don\'t count on it.', 'My reply is no.', ' My sources say no.',
+                      'Outlook not so good.','Very doubtful.']
+keep_asking = True
+
+
 
 while keep_asking:
-    question_content = getQuestion()
-    
-    if question[-1] != "?":
-        print("I’m sorry, I can only answer questions.")
-    elif question == "quit":
+    question = getQuestion()
+
+
+    if question == 'quit':
         keep_asking = False
+    elif question[-1] != "?":
+        print("I’m sorry, I can only answer questions.")
     else:
-        print(random.choice(answerlist))
+        print(random.choice(magic_ball_answers))
